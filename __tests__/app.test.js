f36790afc78846ec6879440dc238dc1f652aaf3a
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-29 16:28:11 -0700

    Auth - Setup express authController

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,16 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+//used later to auth on a tweet post test/route
+//const agent = request.agent(app);
+
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('POST - signs up a user', async () => {
+    const res = await(app).post('')
+  });
+
 });
