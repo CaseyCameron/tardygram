e59246640af5f7af599ce3a2ef62b832ac3fcca8
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-30 12:38:53 -0700

    GREEN - Post POST success

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,57 +2,78 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-
-const agent = request.agent(app);
+import UserService from '../lib/services/userService.js';
 
 describe('demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+  const agent = request.agent(app);
+  let user;
+
+  beforeEach(async () => {
+    await setup(pool);
+
+    //create user here
+    user = await UserService.create({
+      email: 'Bill',
+      profilePhoto: 'photo_url',
+      password: 'password'
+    });
+
+    //login that user here
+    await agent.post('/api/v1/auth/login')
+      .send({
+        email: 'Bill',
+        password: 'password'
+      });
   });
 
   //auth 
-  it.skip('POST - signs up a user', async () => {
+  it('Sign up a user', async () => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
       .send({
-        email: 'signupTest@auth.com',
+        email: 'jimmy',
         profilePhoto: 'photo_url',
         password: 'password'
       });
 
     expect(res.body).toEqual({
-      id: '1',
-      email: 'signupTest@auth.com',
+      id: '2',
+      email: 'jimmy',
       profilePhoto: expect.any(String)
     });
   });
 
-  it.skip('POST - login a user', async () => {
-    const res = await request(app)
+  it('Login a user', async () => {
+    const res = await agent
       .post('/api/v1/auth/login')
       .send({
-        email: 'loginTest@auth.com',
-        profilePhoto: 'photo_url',
+        email: 'Bill',
         password: 'password'
       });
 
     expect(res.body).toEqual({
-      id: '1',
-      email: 'loginTest@auth.com',
+      id: user.id,
+      email: 'Bill',
       profilePhoto: 'photo_url'
     });
   });
 
   //tweets
   it('POSTS a tweet and authenticates the user', async () => {
-    const res = await request(app)
-      .post('/')
-      .send({ text: 'My first tweet' });
+    const res = await agent
+      .post('/api/v1/tweets')
+      .send({
+        photoUrl: 'url',
+        caption: 'caption',
+        tags: []
+      });
 
     expect(res.body).toEqual({
       id: '1',
       userId: '1',
-      text: 'My first tweet'
+      photoUrl: 'url',
+      caption: 'caption',
+      tags: []
     });
   });
 });
