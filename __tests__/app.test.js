cd8167d36477bfd137cbbb06c1eda845b3c5ea14
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-30 09:34:02 -0700

    POST Tweets RED test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,14 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-//used later to auth on a tweet post test/route
-//const agent = request.agent(app);
+const agent = request.agent(app);
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
+  //auth 
   it.skip('POST - signs up a user', async () => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
@@ -27,7 +27,7 @@ describe('demo routes', () => {
     });
   });
 
-  it('POST - login a user', async () => {
+  it.skip('POST - login a user', async () => {
     const res = await request(app)
       .post('/api/v1/auth/login')
       .send({
@@ -40,6 +40,19 @@ describe('demo routes', () => {
       id: '1',
       email: 'loginTest@auth.com',
       profilePhoto: 'photo_url'
+    });
+  });
+
+  //tweets
+  it('POSTS a tweet and authenticates the user', async () => {
+    const res = await request(app)
+      .post('/')
+      .send({ text: 'My first tweet' });
+
+    expect(res.body).toEqual({
+      id: '1',
+      userId: '1',
+      text: 'My first tweet'
     });
   });
 });
