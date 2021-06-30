e6ec49bcaa19e260ddb34ecd10e3caeff2f9f116
CaseyCameron <mcasey.cameron@gmail.com>
2021-06-29 17:48:03 -0700

    RED - POST Login

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,7 +11,7 @@ describe('demo routes', () => {
     return setup(pool);
   });
 
-  it('POST - signs up a user', async () => {
+  it.skip('POST - signs up a user', async () => {
     const res = await request(app)
       .post('/api/v1/auth/signup')
       .send({
@@ -19,6 +19,7 @@ describe('demo routes', () => {
         profilePhoto: 'photo_url',
         password: 'password'
       });
+    
     expect(res.body).toEqual({
       id: '1',
       email: 'signupTest@auth.com',
@@ -26,4 +27,19 @@ describe('demo routes', () => {
     });
   });
 
+  it('POST - login a user', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'loginTest@auth.com',
+        profilePhoto: 'photo_url',
+        password: 'password'
+      });
+      
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'loginTest@auth.com',
+      profielPhoto: 'photo_url'
+    });
+  });
 });
